12727178c40ab9ee73edf4c7490400e673e7a5c2
James Golick <james@james-mbp.local>
2008-05-13 14:10:02 -0400

    include assoc proxy methods module in AR modules on init

--- init.rb
+++ init.rb
@@ -1,1 +1,4 @@
 ActiveRecord::Base.send(:include, HasBrowser)
+[ActiveRecord::Associations::HasManyAssociation, ActiveRecord::Associations::HasManyThroughAssociation].each do |c|
+  c.send(:include, HasBrowser::AssociationProxyMethods)
+end
