1ce15d3a2ac3d57bd37dc381c22c5f99c8663da5
James Golick <james@james-mbp.local>
2008-05-13 12:55:05 -0400

    add a mock, and skeleton for testing

--- lib/has_browser.rb
+++ lib/has_browser.rb
@@ -1,3 +1,11 @@
 module HasBrowser
+  def self.included(receiver)
+    receiver.send :extend, ClassMethods
+  end
   
+  module ClassMethods
+    def has_browser(*args)
+      
+    end
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,10 @@
 require "rubygems"
 require "mocha"
 require "test/unit"
+require File.dirname(__FILE__)+"/../lib/has_browser"
+
+class PhotoMock
+  include HasBrowser
+  
+  has_browser :title, :description, :without_args => [:order_by_date, :order_by_relevance]
+end
